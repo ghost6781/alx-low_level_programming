58a9145b44747d298d17e9858188171732675925
ghost6781 <theo.tmens@gmail.com>
2022-09-15 09:05:39 -0700

    test

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet_x10
+* Description: function uses _putchar 
+* to print alphabetin lowercases for 10 times
+*
+* Return: nothing
+*/
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+	
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
