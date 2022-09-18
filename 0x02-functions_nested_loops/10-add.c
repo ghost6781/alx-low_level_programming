961dbced533f1dc1d4ea31c5266072d9546d5ce9
ghost6781 <theo.tmens@gmail.com>
2022-09-17 18:12:33 -0700

    Task 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - operation that adds two integer values
+ * and returns the result
+ *
+ * @x: first number
+ * @y: second number
+ *
+ * Return: results
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
