ae06cb99d61f3025fe9f33959071c1e8f296f841
ghost6781 <theo.tmens@gmail.com>
2022-09-20 11:27:04 -0700

    test

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts - prints a string to stdout
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, length;
+
+	length = strlen(str);
+
+	/* 
+	 * To traverse string from the end * 
+	*/
+	for (i = lenght - 1; i >= 0; i--)
+	{
+		if str[i] == ' ')
+		{
+			/* 
+			 * put the null character at the
+			 * position of space characters for 
+			 * the next iteration.
+			 */
+			str[i] = '\0';
+			/* 
+			 * start on the next character */
+			_putchar("%s" , &(str[i]) + 1);
+		}
+	}
+
+			/* printing the last word */
+			_putchar("%s", str);
+}
