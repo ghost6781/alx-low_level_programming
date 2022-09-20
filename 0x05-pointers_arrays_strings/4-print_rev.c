c905586b2f6b95125cad06ea45bb15d713f2ef50
ghost6781 <111166573+ghost6781@users.noreply.github.com>
2022-09-20 18:29:21 +0000

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -14,24 +14,27 @@ void print_rev(char *s)
 	length = strlen(str);
 
 	/* 
-	 * To traverse string from the end * 
-	*/
+	 * To traverse string from the end 
+	 */
 	for (i = lenght - 1; i >= 0; i--)
 	{
 		if str[i] == ' ')
 		{
-			/* 
-			 * put the null character at the
-			 * position of space characters for 
-			 * the next iteration.
-			 */
+			/*
+			* put the null character at the
+			* position of space characters for
+			* the next iteration.
+			*/
 			str[i] = '\0';
 			/* 
-			 * start on the next character */
+			 * start on the next character
+			 */
 			_putchar("%s" , &(str[i]) + 1);
 		}
 	}
 
-			/* printing the last word */
+			/*
+			*printing the last word
+			*/
 			_putchar("%s", str);
 }
