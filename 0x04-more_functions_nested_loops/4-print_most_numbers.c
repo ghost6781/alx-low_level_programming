cf4f2250285f93cc053eaf3625d8defc7c75d2ad
ghost6781 <theo.tmens@gmail.com>
2022-10-12 14:01:58 -0700

    Task 4. I beleive in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers from 0 to 9
+ * do not print 2 and 4
+ * can only use _putchar twice in the code
+ * Return: print_most_numbers
+ */
+
+void print_most_numbers(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+	}
+}
