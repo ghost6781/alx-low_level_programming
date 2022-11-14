ee803be4d1ebb85fc29a1142b135355820aefdba
ghost6781 <theo.tmens@gmail.com>
2022-11-14 07:28:28 -0800

    Task 9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,27 +17,19 @@ int main(void)
 	while (x <= 100)
 	{
 		if (x % 15 == 0 && x % 3 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else if (x % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (x % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else
-		{
 			printf("%d", x);
-		}
+
 	       if (x != 100)
-	       { 
-		       printf(" ");
-	       }	       
+		       printf(" ");	       
 		x++;
 	}
 	printf("\n");
+
 	return (0);
 }
